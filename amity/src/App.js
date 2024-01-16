e2ea1ee144db3538c76586835637a01319e7651b
Ravi <63177572+Ravi80595@users.noreply.github.com>
2024-01-17 00:04:59 +0530

    Update App.js

--- amity/src/App.js
+++ amity/src/App.js
@@ -4,25 +4,37 @@ import Dashborad from './Components/Dashborad';
 import Profile from './Components/Profile';
 import AllRoutes from './AllRoutes';
 
-
-
-
 function App() {
+  const containerStyle = {
+    textAlign: 'center',
+    fontFamily: 'Arial, sans-serif',
+    marginTop: '50px',
+  };
 
+  const articleStyle = {
+    maxWidth: '600px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f8f8f8',
+    borderRadius: '8px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  };
 
-
-return (
-    <div className="App">
-      <article>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but we are currently enhancing our software services and preparing for the launch for our mobile application. If you need you can always <a href="mailto:dalbir@riskcare.ca">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>+1 (905) 805-5712</p>
-        <p>&mdash; The Team</p>
-    </div>
-</article>
-        {/* <Dashborad/> */}
-        {/* <Profile/> */}
+  return (
+    <div className="App" style={containerStyle}>
+      <article style={articleStyle}>
+        <h1>We&rsquo;ll be back soon!</h1>
+        <div>
+          <p>
+            Sorry for the inconvenience but we are currently enhancing our software services and preparing for the launch for our mobile application. If you need you can always{' '}
+            <a href="mailto:dalbir@riskcare.ca">contact us</a>, otherwise we&rsquo;ll be back online shortly!
+          </p>
+          <p>+1 (905) 805-5712</p>
+          <p>&mdash; The Team</p>
+        </div>
+      </article>
+      {/* <Dashborad/> */}
+      {/* <Profile/> */}
     </div>
   );
 }
