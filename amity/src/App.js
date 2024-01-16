cc030a5374abe869406ddd0f7132cc493771b304
Ravi <63177572+Ravi80595@users.noreply.github.com>
2024-01-17 00:01:56 +0530

    Update App.js

--- amity/src/App.js
+++ amity/src/App.js
@@ -13,7 +13,14 @@ function App() {
 
 return (
     <div className="App">
-      <AllRoutes/>
+      <article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we are currently enhancing our software services and preparing for the launch for our mobile application. If you need you can always <a href="mailto:dalbir@riskcare.ca">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>+1 (905) 805-5712</p>
+        <p>&mdash; The Team</p>
+    </div>
+</article>
         {/* <Dashborad/> */}
         {/* <Profile/> */}
     </div>
